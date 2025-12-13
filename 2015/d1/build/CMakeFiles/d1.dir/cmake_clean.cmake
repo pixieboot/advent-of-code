5c850d851eb7cac84e5327d3cfcfb66c960c137d
pixieboot <lab_damjanovicn@protonmail.com>
2025-12-13 15:05:12 +0100

    aoc 2015 d1

---
+++ 2015/d1/build/CMakeFiles/d1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/d1.dir/link.d"
+  "CMakeFiles/d1.dir/main.cpp.o"
+  "CMakeFiles/d1.dir/main.cpp.o.d"
+  "d1"
+  "d1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/d1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
