842390ddc7d0a96b6d663e5668ede5eb735e3455
pixieboot <lab_damjanovicn@protonmail.com>
2025-12-28 21:36:22 +0100

    func doc and file err handling added

--- 2015/d2/main.cpp
+++ 2015/d2/main.cpp
@@ -4,7 +4,18 @@
 #include <vector>
 #include <algorithm>
 #include <array>
+#include <cerrno>
+#include <cstring>
 
+/*
+ * Splits string line by the given delimiter
+ *
+ * @param reference s        : string line
+ * @param reference delimiter: delimiter which the string line will be split by
+ * @param reference tokens   : string container vector for element manipulation from 'std::string s'
+ *
+ * @return: void
+ */
 void split(std::string& s, const std::string& delimiter, std::vector<std::string>& tokens) {
     size_t pos = 0;
     std::string token;
@@ -16,7 +27,16 @@ void split(std::string& s, const std::string& delimiter, std::vector<std::string
     tokens.push_back(s);
 };
 
-int sum(std::string& l, std::string& w, std::string& h) {
+/*
+ * Sums total value from the given formula for surface area of the present box
+ *
+ * @param l: length of the box
+ * @param w: width of the box
+ * @param h: height of the box
+ *
+ * @return: total sum from the given formula
+ */
+int surfaceAreaSum(std::string& l, std::string& w, std::string& h) {
     int lenght { std::stoi(l) };
     int width { std::stoi(w) };
     int height { std::stoi(h) };
@@ -42,13 +62,15 @@ int main() {
         while(std::getline(paper, line)) {
             split(line, "x", data);
             for(int i = 0; i < data.size(); i++) {
-                int surfaceArea = sum(data[0], data[1], data[2]);
+                int surfaceArea = surfaceAreaSum(data[0], data[1], data[2]);
                 totalSum += surfaceArea;
                 std::cout << surfaceArea << '\n';
                 std::vector<std::string>().swap(data);
             }
         }
         std::cout << "Total sum for square feet of wrapping paper is: " << totalSum << '\n';
+    } else {
+        std::cerr << "Error: Could not open \"" << std::strerror(errno) << "\"!" << '\n';
     }
     paper.close();
 
